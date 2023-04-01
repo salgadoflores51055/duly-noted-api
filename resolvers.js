3e2770d23d5f5b0637a7675ffc24e77ed1332b4c
Henri Salgado-Flores <salgadoflores51055@gotoltc.edu>
2023-04-01 13:10:41 -0500

    MobDev2 WEEK 10 - Add a resolver for the updateNote, deleteNote Mutuation & archived filter to the notes query.

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,4 @@
-import cuid from 'cuid';
+import cuid from "cuid";
 
 const savedNotes = [];
 
@@ -18,27 +18,78 @@ export default {
         newNote.createdAt = now.toISOString();
       }
 
-      if (!newNote.updatedAt) {
+      if (!newNote.updateAt) {
         const now = new Date();
         newNote.updatedAt = now.toISOString();
       }
 
-      if (typeof newNote.isArchived !== 'boolean') {
+      if (typeof newNote.isArchived !== "boolean") {
         newNote.isArchived = false;
       }
 
       savedNotes.push(newNote);
-
       return newNote;
     },
-  },
+    // Update note 
+    updateNote(_, args) {
+      const { id, note } = args;
 
+      const noteUpdate = savedNotes.find((savedNote) => savedNote.id === id);
+      if (!noteUpdate) {
+        throw new Error('Note with ID ${id} not found');
+      }
+
+      const updateNote = { ...note };
+
+      if (typeof updateNote.isArchived === "boolean" || typeof updateNote.text === "string") {
+        updateNote.updatedAt = (new Date()).toISOString();
+      } else {
+        return noteUpdate;
+      }
+
+      let updatedNote;
+      for (let i = 0; i < savedNotes.length; i++) {
+        if (savedNotes[i].id === id) {
+          Object.assign(savedNotes[i], updateNote);
+          updatedNote = savedNotes[i];
+          break;
+        }
+      }
+
+      return updatedNote;
+    },
+
+    // DELETE NOTE
+
+    deleteNote(_, args) {
+      const { id } = args;
+
+      let deleteNote;
+      for (let i = 0; i < savedNotes.length; i++) {
+        if (savedNotes[i].id === id) {
+          deleteNote = savedNotes.splice(i, 1)[0];
+          break;
+        }
+      }
+      if (!deleteNote) throw new Error(`Note with ID ${id} not found`);
+      return deleteNote;
+
+    }
+
+
+  },
   Query: {
     note(_, args) {
       return savedNotes.find((note) => note.id === args.id);
     },
-    notes() {
-      return savedNotes;
-    },
-  },
+
+    notes(_, args) {
+      const { includeArchived } = args;
+      if (includeArchived) {
+        return savedNotes;
+      }
+
+      return savedNotes.filter((note) => !note.isArchived);
+    }
+  }
 };
